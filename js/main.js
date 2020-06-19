0e17d028243b6cab968135e443b1f17c9f21b205
Amenoxa <48977442+Amenoxa@users.noreply.github.com>
2020-06-19 03:48:12 +0200

    header type effect
    typed.js + opacity on particles

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var doc = jQuery(document);
+
+doc.ready(function(){
+    "use strict";
+
+    var typed = new Typed(".header__text", {
+      strings: ['Profesjonalne podejscie do sportu <b>Mlodziezowego</b>', 'Profesjonalne podejscie do sportu <b>Amatorskiego</b>'],
+      typeSpeed: 30,
+      backSpeed: 30,
+      smartBackspace: true, // this is a default
+      backDelay: 5000,
+      loop: true
+      });
+
+
+})
+
