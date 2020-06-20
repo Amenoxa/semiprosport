b4a62fd21b5ab294dcda97e8b501b9993821bfd0
Amenoxa <48977442+Amenoxa@users.noreply.github.com>
2020-06-20 03:50:33 +0200

    about section
    +partners
    +carousel
    +animated bg

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ doc.ready(function(){
     "use strict";
 
     var typed = new Typed(".header__text", {
-      strings: ['Profesjonalne podejscie do sportu <b>Mlodziezowego</b>', 'Profesjonalne podejscie do sportu <b>Amatorskiego</b>'],
+      strings: ['Profesjonalne podejście do sportu <b>Młodzieżowego</b>', 'Profesjonalne podejście do sportu <b>Amatorskiego</b>'],
       typeSpeed: 30,
       backSpeed: 30,
       smartBackspace: true, // this is a default
@@ -13,6 +13,30 @@ doc.ready(function(){
       });
 
 
+      
+
 
 })
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    lazyLoad: true,
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
 
